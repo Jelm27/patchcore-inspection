66f474b473a3c27af6211367b7e5ff8649f846df
jorgelobaton <jorgeelobaton@gmail.com>
2023-12-11 03:58:38 +0100

    fix dataset

--- bin/run_patchcore.py
+++ bin/run_patchcore.py
@@ -16,8 +16,8 @@ import patchcore.utils
 
 LOGGER = logging.getLogger(__name__)
 
-# _DATASETS = {"mvtec": ["patchcore.datasets.mvtec", "MVTecDataset"]}
-_DATASETS = {"joint": ["patchcore.datasets.joint", "JointDataset"]}
+_DATASETS = {"mvtec": ["patchcore.datasets.mvtec", "MVTecDataset"]}
+# _DATASETS = {"joint": ["patchcore.datasets.joint", "JointDataset"]}
 
 
 @click.group(chain=True)
